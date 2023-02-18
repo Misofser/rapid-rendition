25e9bb3b517782d24b03c51a0a675d1a86a4c174
Sofia <misofser@gmail.com>
2023-02-18 21:41:14 +0300

    The creating of Sessions, Log in, log out

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
 
   get 'signup'  => 'users#new' 
   resources :users
+  
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
 end
