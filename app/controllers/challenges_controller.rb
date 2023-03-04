d9487b2f60793258e276a2075ad45c9fe3384c19
Sofia <misofser@gmail.com>
2023-03-04 19:09:50 +0300

    create a new challenge

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -8,4 +8,23 @@ class ChallengesController < ApplicationController
     @challenge = Challenge.find(params[:id])
   end
 
+  def new
+    @challenge = Challenge.new
+  end
+
+  def create
+    @challenge = Challenge.new(challenge_params)
+
+    if @challenge.save
+      redirect_to @challenge
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def challenge_params
+    params.require(:challenge).permit(:title, :description)
+  end
+
 end
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,4 @@
 class Challenge < ApplicationRecord
   validates :title, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
 end
