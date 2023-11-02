a89d0b750d76c412d77cfb81d7b4bbbec60a5053
Sofia <misofser@gmail.com>
2023-11-02 09:00:20 +0300

    Add demo banner functionality to the app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
     def current_page(path)
         "active" if current_page?(path)
     end
+
+    def show_demo_banner?
+        unless cookies[:demo_banner_seen]
+            return true
+        end
+        
+        false
+    end
+
 end
