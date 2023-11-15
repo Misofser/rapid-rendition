c601192102bfe049f7fe848b7a27d5e1dbea61e2
Sofia <misofser@gmail.com>
2023-11-15 17:38:07 +0300

    Add before_action to require user authentication for show and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user, only: [:show, :destroy]
+  before_action :current_user, only: [:show, :destroy]
 
   def show
     @user = User.find(params[:id])
