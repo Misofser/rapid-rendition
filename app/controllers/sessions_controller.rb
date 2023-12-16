75c49d0f682b400b266726d16bafbfd3f1f124dd
Sofia <misofser@gmail.com>
2023-12-16 15:07:40 +0300

    Add error explanation for login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,18 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      redirect_to '/'
+    if @user
+      if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        redirect_to '/'
+      else
+        flash.now[:danger] = 'Invalid password'
+        render :new
+      end
     else
-      redirect_to '/login'
-    end 
+      flash.now[:danger] = 'User not found for the given email'
+      render :new
+    end
   end 
 
   def destroy 
