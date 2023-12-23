39e4ef178f588f27ea6bd57bc628dfa2cb590e67
Sofia <misofser@gmail.com>
2023-12-23 23:42:20 +0300

    Set image filename generation instead of using the original name in the sketch.

--- app/models/sketch.rb
+++ app/models/sketch.rb
@@ -5,6 +5,7 @@ class Sketch < ApplicationRecord
     has_one_attached :image
     validates :image, presence: true
     validate :validate_image_size
+    before_save :set_custom_filename
 
     def position_in_rankings
         ranking = challenge.sketch_ratings
@@ -17,6 +18,13 @@ class Sketch < ApplicationRecord
     def validate_image_size
       if image.attached? && image.blob.byte_size > 5.megabytes
         errors.add(:image, 'File size exceeds the maximum allowed limit (5 MB)')
+      end
+    end
+
+    def set_custom_filename
+      if image.attached?
+        filename = "#{SecureRandom.hex(16)}.#{image.filename.extension.downcase}"
+        image.blob.update(filename: filename)
       end
     end
 
