589308defa19b8a32e11c6d1a465abb4edaa3e20
Sofia <misofser@gmail.com>
2026-01-18 14:09:44 +0400

    Add validation for mime type

--- app/models/sketch.rb
+++ app/models/sketch.rb
@@ -1,35 +1,44 @@
 class Sketch < ApplicationRecord
-    belongs_to :user
-    belongs_to :challenge
-    has_many :votes, :dependent => :destroy
-    has_one_attached :image
-    validates :image, presence: true
-    validate :validate_image_size
-    before_save :set_custom_filename
+  belongs_to :user
+  belongs_to :challenge
 
-    def vote_count
-      votes.count
-    end
+  has_many :votes, :dependent => :destroy
+  has_one_attached :image
 
-    def position_in_rankings
-        ranking = challenge.sketch_ratings
-        index = ranking.index(self)
-        index ? index + 1 : "Unfortunately, there are currently no votes for this sketch, and it has not been ranked in the competition yet."
-    end
+  validates :image, presence: true
+  validate :validate_image_mime_type
+  validate :validate_image_size
 
-    private
+  before_save :set_custom_filename
+
+  def vote_count
+    votes.count
+  end
+
+  def position_in_rankings
+    ranking = challenge.sketch_ratings
+    index = ranking.index(self)
+    index ? index + 1 : "Unfortunately, there are currently no votes for this sketch, and it has not been ranked in the competition yet."
+  end
+
+  private
   
-    def validate_image_size
-      if image.attached? && image.blob.byte_size > 5.megabytes
-        errors.add(:image, 'File size exceeds the maximum allowed limit (5 MB)')
-      end
+  def validate_image_size
+    if image.attached? && image.blob.byte_size > 5.megabytes
+      errors.add(:image, 'File size exceeds the maximum allowed limit (5 MB)')
     end
+  end
 
-    def set_custom_filename
-      if image.attached?
-        filename = "#{SecureRandom.hex(16)}.#{image.filename.extension.downcase}"
-        image.blob.update(filename: filename)
-      end
+  def validate_image_mime_type
+    if image.attached? && !image.content_type.in?(%w(image/jpeg image/png image/jpg))
+      errors.add(:image, 'File must be a JPEG or PNG')
     end
+  end
 
+  def set_custom_filename
+    if image.attached?
+      filename = "#{SecureRandom.hex(16)}.#{image.filename.extension.downcase}"
+      image.blob.update(filename: filename)
+    end
+  end
 end
